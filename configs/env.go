afccfcda316e57ba7f8fb389140e0131f04ce937
khris-xp <khrisbharmmano@gmail.com>
2023-11-07 13:29:04 +0700

    fix(env): configs for production

--- configs/env.go
+++ configs/env.go
@@ -1,67 +1,33 @@
 package configs
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return os.Getenv("MONGOURI")
 }
 
 func EnvJWTSecret() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return os.Getenv("JWT_SECRET")
 }
 
 func EnvPort() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	return os.Getenv("PORT")
 }
 
 func EnvCloudName() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return os.Getenv("CLOUDINARY_CLOUD_NAME")
 }
 
 func EnvCloudAPIKey() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return os.Getenv("CLOUDINARY_API_KEY")
 }
 
 func EnvCloudAPISecret() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return os.Getenv("CLOUDINARY_API_SECRET")
 }
 
 func EnvCloudUploadFolder() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	return os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
 }
