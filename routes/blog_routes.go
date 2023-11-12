733010a1ebf8fa60c1bd3f857020cb805afd5521
khris-xp <khrisbharmmano@gmail.com>
2023-11-12 22:16:03 +0700

    refac: remove smell code

--- routes/blog_routes.go
+++ routes/blog_routes.go
@@ -1,20 +1,21 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
-
-	"dishdeck-api/controllers"
-	"dishdeck-api/middlewares"
-	"dishdeck-api/repositories"
+    "github.com/gofiber/fiber/v2"
+    "dishdeck-api/controllers"
+    "dishdeck-api/middlewares"
+    "dishdeck-api/repositories"
 )
 
-func BlogRoutes(app *fiber.App) {
-	blogRepo := repositories.NewBlogRepository()
-	blogController := controllers.NewBlogController(blogRepo)
+const BlogAPIPath = "/api/blog/:id"
 
-	app.Post("/api/blog", middlewares.AuthMiddleware(), blogController.CreateBlog)
-	app.Get("/api/blog", blogController.GetAllBlog)
-	app.Get("/api/blog/:id", blogController.GetBlogById)
-	app.Put("/api/blog/:id", middlewares.AuthMiddleware(), blogController.UpdateBlogById)
-	app.Delete("/api/blog/:id", middlewares.AuthMiddleware(), blogController.DeleteBlogById)
+func BlogRoutes(app *fiber.App) {
+    blogRepo := repositories.NewBlogRepository()
+    blogController := controllers.NewBlogController(blogRepo)
+
+    app.Post("/api/blog", middlewares.AuthMiddleware(), blogController.CreateBlog)
+    app.Get("/api/blog", blogController.GetAllBlog)
+    app.Get(BlogAPIPath, blogController.GetBlogById)
+    app.Put(BlogAPIPath, middlewares.AuthMiddleware(), blogController.UpdateBlogById)
+    app.Delete(BlogAPIPath, middlewares.AuthMiddleware(), blogController.DeleteBlogById)
 }
--- routes/menu_routes.go
+++ routes/menu_routes.go
@@ -8,17 +8,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+const MenuAPIPath = "/api/menu/:id"
+
 func MenuRoutes(app *fiber.App) {
 	menuRepo := repositories.NewMenuRepository()
 	menuController := controllers.NewMenuController(menuRepo)
 
 	app.Post("/api/menu", middlewares.AuthMiddleware(), menuController.CreateMenu)
 	app.Get("/api/menu", menuController.GetAllMenu)
-	app.Get("/api/menu/:id", menuController.GetMenuByID)
-	app.Put("/api/menu/:id", middlewares.AuthMiddleware(), menuController.UpdateMenuByID)
-	app.Delete("/api/menu/:id", middlewares.AuthMiddleware(), menuController.DeleteMenuByID)
-	app.Put("/api/menu/:id/like", middlewares.AuthMiddleware(), menuController.LikedMenu)
-	app.Put("/api/menu/:id/unlike", middlewares.AuthMiddleware(), menuController.UnlikedMenu)
-	app.Put("/api/menu/:id/rating", middlewares.AuthMiddleware(), menuController.EditRatingMenu)
-	app.Put("/api/menu/:id/review", middlewares.AuthMiddleware(), menuController.EditReviewMenu)
+	app.Get(MenuAPIPath, menuController.GetMenuByID)
+	app.Put(MenuAPIPath, middlewares.AuthMiddleware(), menuController.UpdateMenuByID)
+	app.Delete(MenuAPIPath, middlewares.AuthMiddleware(), menuController.DeleteMenuByID)
+	app.Put(MenuAPIPath+"/like", middlewares.AuthMiddleware(), menuController.LikedMenu)
+	app.Put(MenuAPIPath+"/unlike", middlewares.AuthMiddleware(), menuController.UnlikedMenu)
+	app.Put(MenuAPIPath+"/rating", middlewares.AuthMiddleware(), menuController.EditRatingMenu)
+	app.Put(MenuAPIPath+"/review", middlewares.AuthMiddleware(), menuController.EditReviewMenu)
 }
